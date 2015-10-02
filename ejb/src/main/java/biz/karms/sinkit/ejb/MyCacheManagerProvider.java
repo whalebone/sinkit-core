00c422462df61e62b3f2d08a1fe5a90cf12a9e89
Michal Karm Babacek <karm@email.cz>
2015-10-02 16:03:20 +0200

    WF10

--- ejb/src/main/java/biz/karms/sinkit/ejb/MyCacheManagerProvider.java
+++ ejb/src/main/java/biz/karms/sinkit/ejb/MyCacheManagerProvider.java
@@ -9,7 +9,6 @@ import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.eviction.EvictionStrategy;
 import org.infinispan.manager.DefaultCacheManager;
-import org.infinispan.persistence.jdbc.configuration.JdbcStringBasedStoreConfigurationBuilder;
 import org.infinispan.transaction.TransactionMode;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -17,6 +16,7 @@ import javax.enterprise.context.Destroyed;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,7 +24,9 @@ import java.util.logging.Logger;
  * @author Michal Karm Babacek
  */
 @ApplicationScoped
-public class MyCacheManagerProvider {
+public class MyCacheManagerProvider implements Serializable {
+
+    private static final long serialVersionUID = 4521683911525257496L;
 
     private static final long ENTRY_LIFESPAN = 4 * 24 * 60 * 60 * 1000; //ms
     private static final long ENTRY_LIFESPAN_NEVER = -1;
@@ -41,17 +43,19 @@ public class MyCacheManagerProvider {
         GlobalConfiguration glob = new GlobalConfigurationBuilder().clusteredDefault() // Builds a default clustered
                 // configuration
                 //.transport().addProperty(JGroupsTransport.CONFIGURATION_FILE, System.getenv("SINKIT_JGROUPS_NETWORKING")) // provide a specific JGroups configuration
-                // .transport().addProperty("configurationFile", "jgroups-udp.xml_bak") // provide a specific JGroups configuration
                 .transport().addProperty("configurationFile", "jgroups-udp.xml")
+                        //.transport().addProperty("configurationFile", "jgroups-tcp.xml")
                         //.transport().defaultTransport()
-                .clusterName("sinkit-cluster")
+                .clusterName("sinkit")
                 .globalJmxStatistics().allowDuplicateDomains(true).enable() // This method enables the jmx statistics of
                         // the global configuration and allows for duplicate JMX domains
                 .build(); // Builds the GlobalConfiguration object
         Configuration loc = new ConfigurationBuilder().jmxStatistics().enable() // Enable JMX statistics
-                //.clustering().cacheMode(CacheMode.DIST_ASYNC)
-                .clustering().cacheMode(CacheMode.REPL_ASYNC)
+                .clustering().cacheMode(CacheMode.DIST_ASYNC)
+                        //.clustering().cacheMode(CacheMode.REPL_ASYNC)
+                        //.async().useReplQueue(true).replQueueInterval(10, TimeUnit.SECONDS)
                         //.hash()//.numOwners(2)
+                        //.locking().useLockStriping(false)
                 .expiration().lifespan(ENTRY_LIFESPAN_NEVER) // Set expiration - cache entries expire after some time (given by
                         // the lifespan parameter) and are removed from the cache (cluster-wide).
                 .expiration().disableReaper()
@@ -63,7 +67,7 @@ public class MyCacheManagerProvider {
                 .transaction().transactionMode(TransactionMode.NON_TRANSACTIONAL)
                         // TODO: Really? Autocommit? -- Yes, autocommit is true by default.
                         //.transactionManagerLookup(new GenericTransactionManagerLookup()).autoCommit(true)
-                .persistence().addStore(JdbcStringBasedStoreConfigurationBuilder.class)
+                /*.persistence().addStore(JdbcStringBasedStoreConfigurationBuilder.class)
                         //.persistence().addStore(JdbcBinaryStoreConfigurationBuilder.class)
                 .fetchPersistentState(true)
                 .ignoreModifications(false)
@@ -80,30 +84,14 @@ public class MyCacheManagerProvider {
                 .driverClass("org.postgresql.Driver")
                 .password(System.getenv("SINKIT_POSTGRESQL_PASS"))
                 .username(System.getenv("SINKIT_POSTGRESQL_USER"))
+                .async()
+                .enabled(true)
+                .threadPoolSize(15)*/
                 .build();
         manager = new DefaultCacheManager(glob, loc, true);
-        manager.defineConfiguration("RULES_CACHE", new ConfigurationBuilder()
-                .eviction()
-                .maxEntries(MAX_ENTRIES_RULES)
-                        // Rules cannot be evicted ever.
-                .expiration().disableReaper()
-                .expiration().lifespan(ENTRY_LIFESPAN_NEVER)
-                        //.clustering().cacheMode(CacheMode.DIST_ASYNC)
-                .clustering().cacheMode(CacheMode.REPL_ASYNC)
-                        //.hash()//.numOwners(2)
-                .indexing().index(Index.ALL)
-                .build());
-        manager.defineConfiguration("CUSTOM_LISTS_CACHE", new ConfigurationBuilder()
-                .eviction()
-                .maxEntries(MAX_ENTRIES_RULES)
-                        // Custom lists cannot be evicted ever.
-                .expiration().disableReaper()
-                .expiration().lifespan(ENTRY_LIFESPAN_NEVER)
-                        //.clustering().cacheMode(CacheMode.DIST_ASYNC)
-                .clustering().cacheMode(CacheMode.REPL_ASYNC)
-                        //.hash()//.numOwners(2)
-                .indexing().index(Index.ALL)
-                .build());
+        manager.defineConfiguration("BLACKLIST_CACHE", loc);
+        manager.defineConfiguration("RULES_CACHE", loc);
+        manager.defineConfiguration("CUSTOM_LISTS_CACHE", loc);
         manager.getCache("BLACKLIST_CACHE").start();
         manager.getCache("RULES_CACHE").start();
         manager.getCache("CUSTOM_LISTS_CACHE").start();
