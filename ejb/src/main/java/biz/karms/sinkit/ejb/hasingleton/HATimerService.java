fa779732141c5d7c2728334134c6a4658e7fddd3
Michal Karm Babacek <karm@email.cz>
2015-10-02 17:01:46 +0200

    Migrated to WF10

--- ejb/src/main/java/biz/karms/sinkit/ejb/hasingleton/HATimerService.java
+++ ejb/src/main/java/biz/karms/sinkit/ejb/hasingleton/HATimerService.java
@@ -18,10 +18,11 @@ package biz.karms.sinkit.ejb.hasingleton;
 
 import biz.karms.sinkit.ejb.IoCDeactivator;
 import biz.karms.sinkit.ejb.virustotal.VirusTotalEnricher;
-import org.jboss.as.server.ServerEnvironment;
-import org.jboss.logging.Logger;
-import org.jboss.msc.service.*;
-import org.jboss.msc.value.Value;
+import org.jboss.msc.service.Service;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.StartContext;
+import org.jboss.msc.service.StartException;
+import org.jboss.msc.service.StopContext;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -30,64 +31,59 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * HATimerService registers various timers
- *
+ * <p>
  * Butchered by:
- * @author Michal Karm Babacek
  *
- * Kudos:
+ * @author Michal Karm Babacek
+ *         <p>
+ *         Kudos:
  * @author Wolf-Dieter Fink
  * @author Ralf Battenfeld
  */
-public class HATimerService implements Service<NodeName> {
+public class HATimerService implements Service<String> {
+    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(HATimerService.class.toString());
+    public static final ServiceName SINGLETON_SERVICE_NAME = ServiceName.JBOSS.append("sinkit", "ha", "singleton", "timer");
     // Timers - JNDI
     public static final String JNDI_PATH_SCHEDULER_DEMO = "global/sinkit-ear/sinkit-ejb/SchedulerDemoBean!biz.karms.sinkit.ejb.hasingleton.SchedulerDemo";
     public static final String JNDI_PATH_VIRUSTOTAL = "global/sinkit-ear/sinkit-ejb/VirusTotalEnricherEJB!biz.karms.sinkit.ejb.virustotal.VirusTotalEnricher";
     public static final String JNDI_PATH_IOCDEACTIVATOR = "global/sinkit-ear/sinkit-ejb/IoCDeactivatorEJB!biz.karms.sinkit.ejb.IoCDeactivator";
 
-    public static final ServiceName DEFAULT_SERVICE_NAME = ServiceName.JBOSS.append("sinkit", "ha", "singleton", "default");
-    public static final ServiceName QUORUM_SERVICE_NAME = ServiceName.JBOSS.append("sinkit", "ha", "singleton", "quorum");
-    // Arbitrary
-    public static final String NODE_1 = "nodeOne";
-    public static final String NODE_2 = "nodeTwo";
-    private static final Logger LOGGER = Logger.getLogger(HATimerService.class);
-    private final Value<ServerEnvironment> env;
+    /**
+     * A flag whether the service is started.
+     */
     private final AtomicBoolean started = new AtomicBoolean(false);
 
-    public HATimerService(Value<ServerEnvironment> env) {
-        this.env = env;
+    /**
+     * @return the name of the server node
+     */
+    public String getValue() throws IllegalStateException, IllegalArgumentException {
+        LOGGER.info(String.format("%s is %s at %s", HATimerService.class.getSimpleName(), (started.get() ? "started" : "not started"), System.getProperty("jboss.node.name")));
+        return "";
     }
 
-    @Override
-    public NodeName getValue() {
-        if (!this.started.get()) {
-            throw new IllegalStateException();
-        }
-        return new NodeName(this.env.getValue().getNodeName());
-    }
-
-    @Override
-    public void start(StartContext context) throws StartException {
+    public void start(StartContext arg0) throws StartException {
         if (!started.compareAndSet(false, true)) {
-            throw new StartException("The service is still started.");
+            throw new StartException("The service is still started!");
         }
-        LOGGER.info("Start HASingleton timer service '" + this.getClass().getName() + "'.");
+        LOGGER.info("Start HASingleton timer service '" + this.getClass().getName() + "'");
+
+        final String node = System.getProperty("jboss.node.name");
         try {
             InitialContext ic = new InitialContext();
             // Timers - Start
-            ((SchedulerDemo) ic.lookup(JNDI_PATH_SCHEDULER_DEMO)).initialize("SCHEDULER_DEMO HASingleton timer @" + this.env.getValue().getNodeName() + " " + new Date());
-            ((VirusTotalEnricher) ic.lookup(JNDI_PATH_VIRUSTOTAL)).initialize("VIRUSTOTAL HASingleton timer @" + this.env.getValue().getNodeName() + " " + new Date());
-            ((IoCDeactivator) ic.lookup(JNDI_PATH_IOCDEACTIVATOR)).initialize("IOCDEACTIVATOR HASingleton timer @" + this.env.getValue().getNodeName() + " " + new Date());
+            ((SchedulerDemo) ic.lookup(JNDI_PATH_SCHEDULER_DEMO)).initialize("SCHEDULER_DEMO HASingleton timer @" + node + " " + new Date());
+            ((VirusTotalEnricher) ic.lookup(JNDI_PATH_VIRUSTOTAL)).initialize("VIRUSTOTAL HASingleton timer @" + node + " " + new Date());
+            ((IoCDeactivator) ic.lookup(JNDI_PATH_IOCDEACTIVATOR)).initialize("IOCDEACTIVATOR HASingleton timer @" + node + " " + new Date());
         } catch (NamingException e) {
-            throw new StartException("Could not initialize timer.", e);
+            throw new StartException("Could not initialize timer", e);
         }
     }
 
-    @Override
-    public void stop(StopContext context) {
+    public void stop(StopContext arg0) {
         if (!started.compareAndSet(true, false)) {
-            LOGGER.warn("The service '" + this.getClass().getName() + "' is not active.");
+            LOGGER.warning("The service '" + this.getClass().getName() + "' is not active!");
         } else {
-            LOGGER.info("Stop HASingleton timer service '" + this.getClass().getName() + "'.");
+            LOGGER.info("Stop HASingleton timer service '" + this.getClass().getName() + "'");
             try {
                 InitialContext ic = new InitialContext();
                 // Timers - Stop
@@ -95,7 +91,7 @@ public class HATimerService implements Service<NodeName> {
                 ((VirusTotalEnricher) ic.lookup(JNDI_PATH_VIRUSTOTAL)).stop();
                 ((IoCDeactivator) ic.lookup(JNDI_PATH_IOCDEACTIVATOR)).stop();
             } catch (NamingException e) {
-                LOGGER.error("Could not stop timer.", e);
+                LOGGER.info("Could not stop timer:" + e.getMessage());
             }
         }
     }
