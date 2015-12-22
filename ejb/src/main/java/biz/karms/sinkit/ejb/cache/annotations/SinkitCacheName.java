cfd27ef60cdb47ad3f0d9a42b82909255fcf7ac2
Michal Karm Babacek <karm@email.cz>
2015-12-22 05:24:25 +0100

    Dockerfile and standalone-ha.xml plus new annotations.

---
+++ ejb/src/main/java/biz/karms/sinkit/ejb/cache/annotations/SinkitCacheName.java
@@ -0,0 +1,8 @@
+package biz.karms.sinkit.ejb.cache.annotations;
+
+/**
+ * @author Michal Karm Babacek
+ */
+public enum SinkitCacheName {
+        BLACKLIST_CACHE, RULES_CACHE, CUSTOM_LISTS_CACHE;
+}
