61ba7c4d222a24963ad97f551a889c2252f0fd49
Michal Karm Babacek <karm@email.cz>
2016-01-24 13:26:06 +0100

    Issue 82, Static contract, plus: DigestUtils.md5Hex and Md5Crypt.md5Crypt instead of our own implementation.

--- ejb/src/main/java/biz/karms/sinkit/ejb/util/WhitelistUtils.java
+++ ejb/src/main/java/biz/karms/sinkit/ejb/util/WhitelistUtils.java
@@ -2,11 +2,8 @@ package biz.karms.sinkit.ejb.util;
 
 import biz.karms.sinkit.ejb.cache.pojo.WhitelistedRecord;
 import biz.karms.sinkit.ioc.IoCRecord;
-import biz.karms.sinkit.ioc.IoCSource;
-import biz.karms.sinkit.ioc.IoCSourceIdType;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,14 +13,9 @@ import java.util.regex.Pattern;
  */
 public class WhitelistUtils {
 
-    public static final String HASH_ALG = "MD5";
-
-    public static String computeHashedId(final String whitelistedId) {
-        return IoCIdentificationUtils.hashString(whitelistedId.getBytes(), HASH_ALG);
-    }
+    public static final Pattern pattern = Pattern.compile("^.*\\.([^\\.]+\\.[^.]+)$");
 
     public static String stripSubdomains(final String fqdn) {
-        Pattern pattern = Pattern.compile("^.*\\.([^\\.]+\\.[^.]+)$");
         Matcher matcher = pattern.matcher(fqdn);
         if (matcher.find()) {
             return matcher.group(1);
@@ -31,10 +23,10 @@ public class WhitelistUtils {
         return fqdn;
     }
 
-    public static WhitelistedRecord createWhitelistedRecord(final IoCRecord iocRecord, boolean completed) {
-        WhitelistedRecord white = new WhitelistedRecord();
+    public static WhitelistedRecord createWhitelistedRecord(final IoCRecord iocRecord, final boolean completed) {
+        final WhitelistedRecord white = new WhitelistedRecord();
         white.setRawId(iocRecord.getSource().getId().getValue());
-        Calendar expiresAt = Calendar.getInstance();
+        final Calendar expiresAt = Calendar.getInstance();
         expiresAt.add(Calendar.SECOND, iocRecord.getSource().getTTL().intValue());
         white.setExpiresAt(expiresAt);
         white.setSourceName(iocRecord.getFeed().getName());
@@ -42,12 +34,12 @@ public class WhitelistUtils {
         return white;
     }
 
-    public static String[] explodeDomains(String fqdn) {
-        String[] explodedFqdn = fqdn.split("\\.");
+    public static String[] explodeDomains(final String fqdn) {
+        final String[] explodedFqdn = fqdn.split("\\.");
         if (explodedFqdn.length == 1) {
             return explodedFqdn;
         }
-        ArrayList<String> fqdns = new ArrayList<>();
+        final ArrayList<String> fqdns = new ArrayList<>();
         String subFqdn = explodedFqdn[explodedFqdn.length - 2] + "." + explodedFqdn[explodedFqdn.length - 1];
         fqdns.add(subFqdn);
         for (int i = explodedFqdn.length - 3; i >= 0; i--) {
